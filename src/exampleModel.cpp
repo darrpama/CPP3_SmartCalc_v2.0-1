476081d19810ce3cb47c3fdcd7d3a1d9be9744a8
darrpama <darrpama@student.21-school.ru>
2023-08-08 15:34:22 +0300

    Writing model

--- src/exampleModel.cpp
+++ src/exampleModel.cpp
@@ -1,21 +1,71 @@
 #include "exampleModel.h"
 
-void ExampleModel::add(double a) 
+void CalculationModel::add(double a) 
 {
     data += a;
 } 
 
-void ExampleModel::mult(double a) 
+void CalculationModel::sub(double a) 
+{
+    data -= a;
+} 
+
+void CalculationModel::mult(double a) 
 {
     data *= a;
 }
 
-void ExampleModel::reset()
+void CalculationModel::div(double a) 
+{
+    data *= (1.0/a);
+}
+
+void CalculationModel::mod(double a) 
+{
+    data = fmod(data, a);
+}
+
+void CalculationModel::degr(double a) 
+{
+    data = pow(data, a);
+}
+
+void CalculationModel::sin(double a) 
+{
+    data = sin(a);
+}
+
+void CalculationModel::cos(double a) 
+{
+    data = pow(data, a);
+}
+
+void CalculationModel::tan(double a) 
+{
+    data = pow(data, a);
+}
+
+void CalculationModel::asin(double a) 
+{
+    data = pow(data, a);
+}
+
+void CalculationModel::acos(double a) 
+{
+    data = pow(data, a);
+}
+
+void CalculationModel::atan(double a) 
+{
+    data = pow(data, a);
+}
+
+void CalculationModel::reset()
 {
     data = 0;
 }
 
-double ExampleModel::getData()
+double CalculationModel::getData()
 {
     return data;
 }
--- src/exampleModel.h
+++ src/exampleModel.h
@@ -1,14 +1,41 @@
 #ifndef EXAMPLEMODEL_H
 #define EXAMPLEMODEL_H
+#include <math.h>
+#include <stack>
+#include <queue>
 
-class ExampleModel
+class CalculationModel
 {
     private:
-        double data;
+        struct Data {
+            double value;
+            int priority;
+            char *oper;
+        };
+        std::stack<Data> stack;
+
+        double answer{};
     public:
-        ExampleModel():data(0){};
+        CalculationModel():answer(0){};
         void add(double a);
+        void sub(double a);
         void mult(double a);
+        void div(double a);
+        void mod(double a);
+        void degr(double a);
+
+        void sin(double a);
+        void cos(double a);
+        void tan(double a);
+        void asin(double a);
+        void acos(double a);
+        void atan(double a);
+
+        void log(double a);
+        void ln(double a);
+
+        void sqrt(double a);
+
         void reset();
         double getData();
 };
